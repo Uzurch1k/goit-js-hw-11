a54b2679a1c60e64f4cb0abe1ef56593c0f0196c
Uzurch1k <galcenkovlad7@gmail.com>
2024-02-06 20:55:36 +0200

    fix

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -8,6 +8,16 @@ import 'izitoast/dist/css/iziToast.min.css';
 
 // ================================================================
 
+const form = document.querySelector('gallery-form');
+const input = document.querySelector('gallery-input');
+const btnSub = document.querySelector('gallery-btn');
+
+const loader = document.querySelector('loader');
+
+const gallery = document.querySelector('gallery-list');
+const galleryItem = document.querySelector('gallery-item');
+const galleryImage = document.querySelector('gallery-image');
+
 // ================================================================
 
 // ================================================================
