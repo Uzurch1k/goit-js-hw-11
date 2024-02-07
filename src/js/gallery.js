252d2ceae7e049a567eb8b9b6f3be4707efccc32
Uzurch1k <galcenkovlad7@gmail.com>
2024-02-07 18:38:40 +0200

    fix

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -8,17 +8,46 @@ import 'izitoast/dist/css/iziToast.min.css';
 
 // ================================================================
 
-const form = document.querySelector('gallery-form');
-const input = document.querySelector('gallery-input');
-const btnSub = document.querySelector('gallery-btn');
-
-const loader = document.querySelector('loader');
-
-const gallery = document.querySelector('gallery-list');
-const galleryItem = document.querySelector('gallery-item');
-const galleryImage = document.querySelector('gallery-image');
+const settingsIziToast = {
+  message: 'Please fill out the input field.',
+  backgroundColor: '#FFA000',
+  titleColor: '#fff',
+  titleSize: '16px',
+  titleLineHeight: '1.5',
+  messageColor: '#fff',
+  messageSize: '16px',
+  messageLineHeight: '1.5',
+  position: 'topRight',
+  timeout: 3000,
+  progressBar: false,
+};
 
 // ================================================================
+
+const form = document.querySelector('.gallery-form');
+const input = document.querySelector('.gallery-input');
+const btnSub = document.querySelector('.gallery-btn');
+
+const loader = document.querySelector('.loader');
+
+const gallery = document.querySelector('.gallery-list');
+const galleryItem = document.querySelector('.gallery-item');
+const galleryImage = document.querySelector('.gallery-image');
+
+// ================================================================
+
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  const text = form.elements.text.value.trim();
+
+  if (!text) {
+    iziToast.warning(settingsIziToast);
+    return;
+  }
+  form.reset();
+}
 
 // ================================================================
 
